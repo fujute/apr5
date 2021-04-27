2afdc7bd61e3f1644f2da28b71333fc77ed2cfd7
fujuTE <fuju@narisa.com>
2021-04-27 17:18:22 +0700

    Add secret yaml

--- nginx-php/index.php
+++ nginx-php/index.php
@@ -1,3 +1,11 @@
+<html>
+<body>
+<h2> Hello NGINX / PHP </h2>
 <?php
-phpinfo();
+$ip = getenv('REMOTE_ADDR', true) ?: getenv('REMOTE_ADDR');
+echo $ip;
+
+$mysql-server = getenv('MYSQL_HOSTNAME', true) ?: getenv('MYSQL_HOSTNAME');
 ?>
+</body>
+</html>
